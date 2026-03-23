c58ad0aa716b6835a22f701308ca8d3c7fd4112a
Anon Ymous <ancientstraits@protonmail.com>
2026-03-23 16:08:04 -0400

    MADE THIS FLIPPING PARALLEL FLLLLLLIIIIIIIIIIIPPPPPPPPPPPPPP

--- Views/DiscoverView.axaml.cs
+++ Views/DiscoverView.axaml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
+using Avalonia.Threading;
 using HarfBuzzSharp;
 
 namespace mmm;
@@ -17,37 +18,41 @@ public partial class DiscoverView : UserControl
         _ = LoadFeaturedAsync();
     }
 
+    private async Task AddSubmissionPanel(Record record)
+    {
+        var SubmissionPanel = new SubmissionPanel();
+        SubmissionPanel.DataContext = record;
+
+        foreach (string tag in record.Tags)
+        {   
+            var tagBlock = new TextBlock();
+            tagBlock.Text = tag;
+            SubmissionPanel.SubmissionTags.Children.Add(tagBlock);
+        }
+
+        var ThumbnailUrl = new Uri(GameBanana.GetSubmissionImageUrlByImageSize(record.PreviewMedia.Images[0], GameBanana.ImageSizes.Size220));
+        var ThumbnailBitmap = await ImageHelper.LoadFromWeb(ThumbnailUrl);
+
+        SubmissionPanel.SubmissionThumbnail.Background = new ImageBrush
+        {
+            Source = ThumbnailBitmap,
+            Stretch = Stretch.UniformToFill,
+            AlignmentX = AlignmentX.Center,
+            AlignmentY = AlignmentY.Top
+        };
+
+        if (record.HasFiles)
+        {
+            SubmissionsContainer.Children.Add(SubmissionPanel);
+        }
+    }
+
     private async Task LoadFeaturedAsync()
     {
         SubmissionItem jsonResponse = await GameBanana.GetFeaturedSubmissions(GameBanana.client);
-        //TODO: MAKE THIS FLIPPING PARALLEL FLIIIIIIIIIIIPPPPPPPP
         foreach (Record record in jsonResponse.Records)
         {
-            var SubmissionPanel = new SubmissionPanel();
-            SubmissionPanel.DataContext = record;
-
-            foreach (string tag in record.Tags)
-            {   
-                var tagBlock = new TextBlock();
-                tagBlock.Text = tag;
-                SubmissionPanel.SubmissionTags.Children.Add(tagBlock);
-            }
-
-            var ThumbnailUrl = new Uri(GameBanana.GetSubmissionImageUrlByImageSize(record.PreviewMedia.Images[0], GameBanana.ImageSizes.Size220));
-            var ThumbnailBitmap = await ImageHelper.LoadFromWeb(ThumbnailUrl);
-
-            SubmissionPanel.SubmissionThumbnail.Background = new ImageBrush
-            {
-                Source = ThumbnailBitmap,
-                Stretch = Stretch.UniformToFill,
-                AlignmentX = AlignmentX.Center,
-                AlignmentY = AlignmentY.Top
-            };
-
-            if (record.HasFiles)
-            {
-                SubmissionsContainer.Children.Add(SubmissionPanel);
-            }
-        };
+            Dispatcher.UIThread.Post(async () => await AddSubmissionPanel(record));
+        }
     }
 }
